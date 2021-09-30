aedba92f4a93c0c402115747f4e2198ea49ddc61
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-09-30 22:05:02 +0530

    added port config to bootstrap

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(8080);
+    await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
