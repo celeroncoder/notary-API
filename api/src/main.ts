3460d52f21d1dabe10182f9b33ca6dc14a3d1ede
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-09-30 22:06:06 +0530

    converted bootstap to iife

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,8 +4,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-async function bootstrap() {
+(async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT || 8080);
-}
-bootstrap();
+})();
