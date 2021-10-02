e649ea3bff77d57d0efdb64d0fa3822f23f04cb8
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-10-02 11:42:55 +0530

    added explicit types

--- src/notes/validate-objectId.pipe.ts
+++ src/notes/validate-objectId.pipe.ts
@@ -10,8 +10,8 @@ import * as mongoose from "mongoose";
 const ObjectId = mongoose.Types.ObjectId;
 
 @Injectable()
-export class ValidateObjectId implements PipeTransform {
-    transform(value: string, _metadata: ArgumentMetadata) {
+export class ValidateObjectId implements PipeTransform<string, string> {
+    transform(value: string, _metadata: ArgumentMetadata): string {
         if (ObjectId.isValid(value)) {
             if (String(new ObjectId(value)) === value) return value;
             throw new BadRequestException("id is not valid.");
