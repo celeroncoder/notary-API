3b3171939b2777ede6e2dd3c2a7375e171162361
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-10-10 20:38:30 +0530

    feat: fixed test dependencies

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UsersService } from "./users.service";
+import config from "../config/keys"
+import { UserSchema } from "./models/user.schema";
+import { MongooseModule } from "@nestjs/mongoose";
 
 describe("UsersService", () => {
 	let service: UsersService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
+			imports: [
+				MongooseModule.forRoot(config.mongoURI),
+				MongooseModule.forFeature([{ name: "User", schema: UserSchema }]),
+			],
 			providers: [UsersService],
 		}).compile();
 
