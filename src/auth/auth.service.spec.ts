06bdbdf2d053aa2c027c888343a89515ec82bb1f
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-10-10 20:38:27 +0530

    feat: fixed test dependencies

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,20 @@
+import { MongooseModule } from "@nestjs/mongoose";
+import { PassportModule } from "@nestjs/passport";
 import { Test, TestingModule } from "@nestjs/testing";
+import { UserSchema } from "../users/models/user.schema";
+import { UsersModule } from "../users/users.module";
 import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./local.strategy";
+import config from "../config/keys";
 
 describe("AuthService", () => {
 	let service: AuthService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [AuthService],
+			imports: [UsersModule, PassportModule, MongooseModule.forRoot(config.mongoURI),
+				MongooseModule.forFeature([{ name: "User", schema: UserSchema }]),],
+			providers: [AuthService, LocalStrategy],
 		}).compile();
 
 		service = module.get<AuthService>(AuthService);
