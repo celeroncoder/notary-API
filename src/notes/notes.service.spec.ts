1b936e9df69438b9052943a3c87deb37576d54e0
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-10-10 20:16:52 +0530

    updated notes test

--- src/notes/notes.service.spec.ts
+++ src/notes/notes.service.spec.ts
@@ -1,11 +1,20 @@
+import { MongooseModule } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
+import { NoteSchema } from "./models/note.schema";
 import { NotesService } from "./notes.service";
+import config from "../config/keys";
 
 describe("NotesService", () => {
 	let service: NotesService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
+			imports: [
+				MongooseModule.forFeature([
+					{ name: "Note", schema: NoteSchema },
+				]),
+				MongooseModule.forRoot(config.mongoURI),
+			],
 			providers: [NotesService],
 		}).compile();
 
