d08177babd81e1c6b72678e6dd7248be683107d2
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-10-16 13:42:28 +0530

    chore: added typeorm config

---
+++ src/config/typeorm/typeorm.dev.ts
@@ -0,0 +1,19 @@
+import { Users } from "src/users/entities/users.entity";
+import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
+
+const typeOrmDevOptions: MysqlConnectionOptions = {
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "notary-db",
+    synchronize: true,
+    logging: false,
+    entities: [Users],
+    cli: {
+        migrationsDir: "src/migrations",
+    },
+};
+
+export default typeOrmDevOptions;
---
+++ src/config/typeorm/typeorm.prod.ts
@@ -0,0 +1,22 @@
+import { Users } from "src/users/entities/users.entity";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+const typeOrmProdOptions: PostgresConnectionOptions = {
+    
+	type: "postgres",
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    logging: false,
+    entities: [Users],
+    cli: {
+        migrationsDir: "src/migrations",
+    },
+	ssl: {
+		rejectUnauthorized: false
+	}
+};
+
+export default typeOrmProdOptions;
