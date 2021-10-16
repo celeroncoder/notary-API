79e580b7cc72096d1acf059a0480adcf8d1afc69
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-10-16 16:30:13 +0530

    feat: added support for container service

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,18 @@ import * as helmet from "helmet";
 import * as compression from "compression";
 import { ValidationPipe } from "@nestjs/common";
 import { globalValidationPipeOptions } from "./config/validation-pipe";
+import { useContainer } from "class-validator";
 
 declare const module: any;
 
 (async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
     app.enableCors();
     app.use(helmet());
-	app.use(compression());
+    app.use(compression());
 
     app.setGlobalPrefix("api");
     app.useGlobalPipes(new ValidationPipe(globalValidationPipeOptions));
